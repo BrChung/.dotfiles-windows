0e48367dda8dd2529f788eaeef87ab97adc11c32
Brian Chung <brian@brianchung.co>
2023-06-21 16:30:55 -0700

    Initial Commit

---
+++ Bootstrap.ps1
@@ -0,0 +1,32 @@
+# Based on https://github.com/JMOrbegoso/Dotfiles-for-Windows-11
+
+$GitHubAuthor = "BrChung"
+$GitHubRepo = ".dotfiles-windows"
+$GitHubBranch = "main"
+$GitHubRepoUri = "https://github.com/${GitHubAuthor}/${GitHubRepo}/archive/refs/heads/${GitHubBranch}.zip";
+
+$DotfilesDir = Join-Path -Path $HOME -ChildPath "${$GitHubRepo}";
+$DotfilesSrcDir = Join-Path -Path $DotfilesDir -ChildPath "src";
+$RepoZip = Join-Path -Path $HOME -ChildPath "${$GitHubRepo}.zip";
+
+# Create Dotfiles folder
+if (Test-Path $DotfilesDir) {
+  Remove-Item -Path $DotfilesDir -Recurse -Force;
+}
+New-Item $DotfilesDir -ItemType directory;
+
+# Download Dotfiles repository as Zip
+$DownloadResult = $FALSE;
+Try {
+  Invoke-WebRequest $GitHubRepoUri -O $RepoZip;
+  $DownloadResult = $TRUE;
+}
+catch [System.Net.WebException] {
+  Write-Host "Error connecting to GitHub, check the internet connection or the repository url." -ForegroundColor "Red";
+}
+
+if ($DownloadResult) {
+  Add-Type -AssemblyName System.IO.Compression.FileSystem;
+  [System.IO.Compression.ZipFile]::ExtractToDirectory($RepoZip, $HOME);
+  Invoke-Expression (Join-Path -Path $DotfilesSrcDir -ChildPath "Setup.ps1");
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# .dotfiles-windows
- 
+# Dotfiles for Windows
+
+## Usage
+
+```Powershell
+$GitHubAuthor = "BrChung"; `
+$GitHubRepo = ".dotfiles-windows"; `
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass; `
+Invoke-Expression (Invoke-RestMethod -Uri "https://raw.githubusercontent.com/${GitHubRepositoryAuthor}/${GitHubRepositoryName}/main/Bootstrap.ps1");
+```
