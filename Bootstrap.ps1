abbb254988537fc17fae12e66ccf51873f3ba772
Brian Chung <brian@brianchung.co>
2023-06-21 16:44:31 -0700

    Update Bootstrap.ps1

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -5,32 +5,28 @@ $GitHubRepo = ".dotfiles-windows"
 $GitHubBranch = "main"
 $GitHubRepoUri = "https://github.com/${GitHubAuthor}/${GitHubRepo}/archive/refs/heads/${GitHubBranch}.zip";
 
-Write-Host "The following is the dotfiles repo: ${GitHubRepo}" -ForegroundColor "White";
-
 $DotfilesDir = Join-Path -Path $HOME -ChildPath "${GitHubRepo}";
 $DotfilesSrcDir = Join-Path -Path $DotfilesDir -ChildPath "src";
 $RepoZip = Join-Path -Path $HOME -ChildPath "${GitHubRepo}.zip";
 
-Write-Host "The following is the dotfiles path: ${DotfilesDir}" -ForegroundColor "White";
+# Create Dotfiles folder
+if (Test-Path $DotfilesDir) {
+  Remove-Item -Path $DotfilesDir -Recurse -Force;
+}
+New-Item $DotfilesDir -ItemType directory;
 
-# # Create Dotfiles folder
-# if (Test-Path $DotfilesDir) {
-#   Remove-Item -Path $DotfilesDir -Recurse -Force;
-# }
-# New-Item $DotfilesDir -ItemType directory;
+# Download Dotfiles repository as Zip
+$DownloadResult = $FALSE;
+Try {
+  Invoke-WebRequest $GitHubRepoUri -O $RepoZip;
+  $DownloadResult = $TRUE;
+}
+catch [System.Net.WebException] {
+  Write-Host "Error connecting to GitHub, check the internet connection or the repository url." -ForegroundColor "Red";
+}
 
-# # Download Dotfiles repository as Zip
-# $DownloadResult = $FALSE;
-# Try {
-#   Invoke-WebRequest $GitHubRepoUri -O $RepoZip;
-#   $DownloadResult = $TRUE;
-# }
-# catch [System.Net.WebException] {
-#   Write-Host "Error connecting to GitHub, check the internet connection or the repository url." -ForegroundColor "Red";
-# }
-
-# if ($DownloadResult) {
-#   Add-Type -AssemblyName System.IO.Compression.FileSystem;
-#   [System.IO.Compression.ZipFile]::ExtractToDirectory($RepoZip, $HOME);
-#   Invoke-Expression (Join-Path -Path $DotfilesSrcDir -ChildPath "Setup.ps1");
-# }
+if ($DownloadResult) {
+  Add-Type -AssemblyName System.IO.Compression.FileSystem;
+  [System.IO.Compression.ZipFile]::ExtractToDirectory($RepoZip, $HOME);
+  Invoke-Expression (Join-Path -Path $DotfilesSrcDir -ChildPath "Setup.ps1");
+}
